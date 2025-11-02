24c212de900af246bb79fe38a7ab29029dde83c6
Mohamed-ryad LAOUEDJ <mlaouedj@f2r4s6.clusters.42paris.fr>
2025-11-02 18:42:51 +0100

    protected supabase client

--- my-react-app/src/supabaseClient.ts
+++ my-react-app/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://wkpekfxthkmyemxashbc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrcGVrZnh0aGtteWVteGFzaGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTI1MzIsImV4cCI6MjA3NzQ4ODUzMn0.FgrLIOAcvzgKZzliy3WpLsALX0ce2f0zNGbWZs8D3Xw';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
